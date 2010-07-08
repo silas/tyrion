e06255d0eecb600714d1dfeaa91ff1ed208b201f
Silas Sewell <silas@sewell.ch>
2010-07-07 22:49:25 -0600

    Update copyright to use AUTHORS file

--- src/config.cc
+++ src/config.cc
@@ -1,7 +1,7 @@
 /*
  * Tyrion
- * Copyright 2009, Brush Technology
- * Copyright 2010, Silas Sewell
+ * Copyright 2009 Brush Technology
+ * Copyright 2010 The Tyrion Authors. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -11,7 +11,7 @@
  *  2. Redistributions in binary form must reproduce the above copyright notice,
  *     this list of conditions and the following disclaimer in the documentation
  *     and/or other materials provided with the distribution.
- *  3. The name of the author may not be used to endorse or promote products
+ *  3. The names of the authors may not be used to endorse or promote products
  *     derived from this software without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
