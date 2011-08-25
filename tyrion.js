405762341b81c312f0e234dc9a0459dc6bf7b785
Silas Sewell <silas@sewell.org>
2011-08-24 21:51:36 -0400

    Spaces vs tabs

--- tyrion.js
+++ tyrion.js
@@ -4,7 +4,7 @@ var amqp = require('amqp')
 
 var defaults = {
   config_file: {
-	abbr: 'c',
+    abbr: 'c',
     full: 'config-file',
     help: 'Configuration file',
   },
@@ -29,31 +29,31 @@ var defaults = {
 var options = nomnom.scriptName('tyrion').opts(defaults).parseArgs()
 
 if (options.config_file) {
-	var config = iniparser.parseSync(options.config_file)
+  var config = iniparser.parseSync(options.config_file)
 
-	for (var key in defaults) {
-		var v = defaults[key].full.split('-', 2)
-		var k1 = v[0]
-		  , k2 = v[1]
-		if (!options[key]) {
-			if (config[k1] && config[k1][k2]) {
-				if ((defaults[key].type || 'string') != 'string') {
-					try {
-						options[key] = JSON.parse(config[k1][k2])
-					} catch(e) {
-					}
-				} else {
-					options[key] = config[k1][k2]
-				}
-			} else {
-				options[key] = defaults[key]._default
-			}
-		}
-	}
+  for (var key in defaults) {
+    var v = defaults[key].full.split('-', 2)
+    var k1 = v[0]
+      , k2 = v[1]
+    if (!options[key]) {
+      if (config[k1] && config[k1][k2]) {
+        if ((defaults[key].type || 'string') != 'string') {
+          try {
+            options[key] = JSON.parse(config[k1][k2])
+          } catch(e) {
+          }
+        } else {
+          options[key] = config[k1][k2]
+        }
+      } else {
+        options[key] = defaults[key]._default
+      }
+    }
+  }
 } else {
-	for (var key in defaults) {
-		options[key] = options[key] || defaults[key]._default
-	}
+  for (var key in defaults) {
+    options[key] = options[key] || defaults[key]._default
+  }
 }
 
 var connection = amqp.createConnection({ host: options.amqp_host, port: options.amqp_port })
@@ -62,7 +62,7 @@ connection.on('ready', function () {
   connection.exchange(options.amqp_exchange, {type: 'topic'}, function(exchange) {
     connection.queue('', function(q) {
       q.bind(exchange, "*")
-
+      
       q.on('queueBindOk', function() {
         q.subscribe(function (message) {
           console.log('Got message: ' + message.data.toString())
